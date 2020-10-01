b2f032de0108003c2eca7b727ad57b7fb25b4fd1
felper <30592974+felper@users.noreply.github.com>
2020-10-01 15:55:46 +0300

    Create students.db.sql

---
+++ week 7/houses/students.db.sql
@@ -0,0 +1,9 @@
+CREATE TABLE students (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first VARCHAR(255),
+    middle VARCHAR(255),
+    last VARCHAR(255),
+    house VARCHAR(10),
+    birth INTEGER
+);
+CREATE TABLE sqlite_sequence(name,seq);
